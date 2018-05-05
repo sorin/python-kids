4c472e8a47b74f2cb748baa3dfb484d8e8da87a0
Sorin Neacsu <sorin.neacsu@gmail.com>
2018-05-05 12:32:37 -0700

    Turtle star

---
+++ turtle/star.py
@@ -0,0 +1,22 @@
+import turtle 
+
+painter = turtle.Turtle()
+
+painter.pencolor("blue")
+
+for i in range(50):
+    painter.forward(50)
+    painter.left(123) # Let's go counterclockwise this time 
+    
+painter.pencolor("red")
+for i in range(50):
+    painter.forward(100)
+    painter.left(123)
+    
+painter.pencolor("green")
+for i in range(50):
+    painter.forward(200)
+    painter.left(123)
+
+turtle.done()
+
